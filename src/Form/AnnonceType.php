868d3eb0f25ecdbd9bfe61172bf00c6ee7548904
Elneris <elneris.dang@gmail.com>
2019-08-25 21:24:01 +0200

    Gestion des annonces et edition des annonces

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -17,16 +18,17 @@ class AnnonceType extends AbstractType
     /**
      * @param string $label
      * @param string $placeholder
+     * @param array $option
      * @return array
      */
-    private function getConfiguration($label, $placeholder)
+    private function getConfiguration($label, $placeholder, $option = [])
     {
-        return [
+        return array_merge([
             'label' => $label,
             'attr' => [
                 'placeholder' => $placeholder
             ]
-        ];
+        ], $option);
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -35,7 +37,9 @@ class AnnonceType extends AbstractType
             ->add('title', TextType::class,
                 $this->getConfiguration("Titre", "Tapez un titre pour votre annonce"))
             ->add('slug', TextType::class,
-                $this->getConfiguration("Adresse web", "Tapez l'adresse web (automatique)"))
+                $this->getConfiguration("Adresse web", "Tapez l'adresse web (automatique)", [
+                    'required' => false
+                ]))
             ->add('coverImage', UrlType::class,
                 $this->getConfiguration("Url de l'image", "Donnez l'url de l'image de votre appartement"))
             ->add('introduction', TextType::class,
@@ -46,6 +50,11 @@ class AnnonceType extends AbstractType
                 $this->getConfiguration("Nombre de chambres", "Nombre de chambres disponibles"))
             ->add('price', MoneyType::class,
                 $this->getConfiguration("Prix par nuit", "Indiquez le prix que vous voulez pour une nuit"))
+            ->add('images', CollectionType::class, [
+                'entry_type' => ImageType::class,
+                'allow_add' => true,
+                'allow_delete' => true
+            ])
         ;
     }
 
