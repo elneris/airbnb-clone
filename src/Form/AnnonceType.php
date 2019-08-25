d5607f244c4baa5bbd5ad7e07b2118edd6bd4a58
Elneris <elneris.dang@gmail.com>
2019-08-25 17:20:06 +0200

    création des annonces

---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
+{
+    /**
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder)
+    {
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ];
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class,
+                $this->getConfiguration("Titre", "Tapez un titre pour votre annonce"))
+            ->add('slug', TextType::class,
+                $this->getConfiguration("Adresse web", "Tapez l'adresse web (automatique)"))
+            ->add('coverImage', UrlType::class,
+                $this->getConfiguration("Url de l'image", "Donnez l'url de l'image de votre appartement"))
+            ->add('introduction', TextType::class,
+                $this->getConfiguration("Introduction", "Donnez une description de l'annonce"))
+            ->add('content', TextareaType::class,
+                $this->getConfiguration("Description détaillée", "Tapez une description qui donne envie de venir chez vous !"))
+            ->add('rooms', IntegerType::class,
+                $this->getConfiguration("Nombre de chambres", "Nombre de chambres disponibles"))
+            ->add('price', MoneyType::class,
+                $this->getConfiguration("Prix par nuit", "Indiquez le prix que vous voulez pour une nuit"))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+        ]);
+    }
+}
